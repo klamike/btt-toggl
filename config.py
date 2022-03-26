63087ae82061f7be152723957a75118190964f16
Klamkin, Michael <mike@klamkin.com>
2022-03-25 21:09:39 -0400

    add back example wid_pid_dict

--- config.py
+++ config.py
@@ -9,6 +9,11 @@ PATH_TO_CACHE_FILE = '/path/to/cache/status.json'
 
 # Dictionary mapping workspace and project to display name
 # WID_PID_DICT = {<wid>: {<pid>: <display name>, ...}, ...}
+WID_PID_DICT = {1000000:{100000001:'W1 P1',
+                         100000002:'W1 P2',
+                         100000003:'W1 P3'},
+                2000000:{200000001:'W2 P1',
+                         200000002:'W2 P2'}}
 
 # by default, btt-toggl will apply the tag "btt-toggl" to all entries it creates
 TAG_ALL_ENTRIES = True
